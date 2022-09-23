6bf7f26b39c575fa0eaefb59f1ebf1946d0cd98d
Noah Beito <noah.beito@gmail.com>
2022-09-23 12:06:31 -0700

    add random quote generator files

---
+++ quotes.js
@@ -0,0 +1,30 @@
+const QUOTEBANK = [
+    {
+        quote: "People think that atheists have nothing to live for, but that's not true - it's that we have nothing to die for.",
+        author: "Matthew Healy"
+    },
+    {
+        quote: "Life's too short to drink crappy coffee and cry over boys who don't care.",
+        author: "Matthew Healy"
+    },
+    {
+        quote: "I've got to be honest mate, I fucking hate fishing...",
+        author: "Fisher"
+    },
+    {
+        quote: "Nobody does it better. Six foot eight, 37 inch vertical leap and you're telling me that you got the guts to go against me. You've convinced yourself that you're better than me! Wrong. You're not even close, cause I'm at the tippity top. I'm at the tippity top of the mountain, and I'm literally only halfway up.",
+        author: "Dr. Disrespect"
+    },
+    {
+        quote: "And you think I'm gunna stop right there? You think that's the end of the line? You think we're done? That we put the locks on the arena? Champions Club, we're just getting started.",
+        author: "Dr. Disrespect"
+    },
+    {
+        quote: "Don't you dare peak Johnny boy. Don't you ever dare peak in the Two Time's world. This is my videogame realm, this is my videogame domain.",
+        author: "Dr. Disrespect"
+    },
+    {
+        quote: "Dark alleyways is where they're gunna find us. The next game is what we're going to get into. The next play, it's going to be a dominating one. The next snipe, 360, minimum. The next victory, celebration time.",
+        author: "Dr. Disrespect" 
+    }
+]
